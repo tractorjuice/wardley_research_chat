4688b5962761f306c0d72204393ea291123c7f2a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-07-06 10:03:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from langchain.chains import RetrievalQAWithSourcesChain
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
-
 from langchain.vectorstores import Pinecone
 import pinecone
 
@@ -71,9 +70,6 @@ messages = [
 ]
 prompt = ChatPromptTemplate.from_messages(messages)
 
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
-
 chain_type_kwargs = {"prompt": prompt}
 llm = ChatOpenAI(model_name="gpt-3.5-turbo-16k-0613", temperature=0, max_tokens=256)  # Modify model_name if you have access to GPT-4
 chain = RetrievalQAWithSourcesChain.from_chain_type(
@@ -84,38 +80,8 @@ chain = RetrievalQAWithSourcesChain.from_chain_type(
     chain_type_kwargs=chain_type_kwargs
 )
 
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
-if os.path.exists(DATA_STORE_DIR):
-    vector_store = FAISS.load_local(
-        DATA_STORE_DIR,
-        OpenAIEmbeddings()
-    )
-else:
-    st.write(f"Missing files. Upload index.faiss and index.pkl files to {DATA_STORE_DIR} directory first")
-
-system_template="""Use the following pieces of context to answer the users question.
-If you don't know the answer, just say that "I don't know", don't try to make up an answer.
-----------------
-{summaries}"""
-prompt_messages = [
-    SystemMessagePromptTemplate.from_template(system_template),
-    HumanMessagePromptTemplate.from_template("{question}")
-    ]
-prompt = ChatPromptTemplate.from_messages(prompt_messages)
-
-chain_type_kwargs = {"prompt": prompt}
-llm = ChatOpenAI(model_name=MODEL, temperature=0, max_tokens=300)  # Modify model_name if you have access to GPT-4
-print("done chain stuff")
-chain = RetrievalQAWithSourcesChain.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vector_store.as_retriever(),
-    return_source_documents=True,
-    chain_type_kwargs=chain_type_kwargs
-)
 
 #print(result['question'],'\n')
 #print(result['answer'],'\n')
