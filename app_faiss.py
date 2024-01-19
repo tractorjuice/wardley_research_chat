52c572b42992131e3c08f8d741a768913497542a
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-01-19 13:16:24 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -1,19 +1,17 @@
 import os
 import re
 import uuid
-import openai
+from langchain_openai import OpenAI
 import promptlayer
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import PromptLayerChatOpenAI
-from langchain.vectorstores import FAISS
+from langchain_community.chat_models import PromptLayerChatOpenAI
+from langchain_community.vectorstores import FAISS
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
 MODEL = "gpt-3.5-turbo-16k"
-#MODEL = "gpt-4-vision-preview"
 
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
