929a59f0eb9cd5fa81fb5dd73449bbe22193800b
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-01-27 19:00:38 +0000

    Update app_faiss.py

--- app_faiss.py
+++ app_faiss.py
@@ -12,11 +12,14 @@ from langchain_community.chat_models import PromptLayerChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 
-#MODEL = "gpt-3.5-turbo-16k" # Legacy
-#MODEL = "gpt-3.5-turbo-1106" # Current model
-MODEL = "gpt-3.5-turbo-0125" # Latest model
-#MODEL = "gpt-4-0125-preview" Latest model
-DEBUG = True # True to overwrite files that already exist
+# gpt-3.5-turbo, gpt-4, and gpt-4-turbo-preview point to the latest model version
+#MODEL = "gpt-3.5-turbo" # 4K, Sept 2021
+#MODEL = "gpt-3.5-turbo-16k" # 16K, Sept 2021
+MODEL = "gpt-3.5-turbo-1106" # 16K, Sept 2021
+#MODEL = "gpt-4" # 8K, Sept 2021
+#MODEL = "gpt-4-32k" # 32K, Sept 2021
+#MODEL = "gpt-4-turbo-preview" # 128K, Apr 2023
+#MODEL = "gpt-4-1106-preview" # 128K, Apr 2023
 
 if "session_id" not in st.session_state:
     st.session_state.session_id = str(uuid.uuid4())
